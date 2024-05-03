f6225e6ad50d2bf244bb8721921cd56a08da88be
George Davidson <georgedavidson392@gmail.com>
2024-05-02 21:53:50 -0400

    updated particle sys

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #define DRAG 0.984
 #define THRUST 2
 
-#define MAX_PARTICLES 40
+#define MAX_PARTICLES 200
 
 typedef struct particle {
     float radius;
@@ -14,6 +14,14 @@ typedef struct particle {
     Vector2 position;
     float shrinkRate;
 } particle_t;
+
+// float min(float a, float b) {
+//     return (a < b) ? a : b;
+// }
+
+float max(float a, float b) {
+    return (a > b) ? a : b;
+}
 
 int main() {
     const int winWidth = 800;
@@ -34,12 +42,7 @@ int main() {
 
     particle_t particles[MAX_PARTICLES];
     for (int i = 0; i < MAX_PARTICLES - 1; i++) { 
-        particles[i].radius = GetRandomValue(3, 5);
-        particles[i].velocity.x = GetRandomValue(-30, 30) / 10.0f;
-        particles[i].velocity.y = GetRandomValue(10, 100) / 10.0f;
-        particles[i].position.x = rect.x;
-        particles[i].position.y = rect.y;
-        particles[i].shrinkRate = GetRandomValue(1, 10) / 10.0f;
+        particles[i].radius = 0.0f;
     }
 
     SetTargetFPS(60);
@@ -69,9 +72,13 @@ int main() {
 
         for (int i = 0; i < MAX_PARTICLES - 1; i++) {             
             if (particles[i].radius <= 0.0f) {
-                particles[i].radius = GetRandomValue(3, 5);
-                particles[i].velocity.x = GetRandomValue(-30, 30) / 10.0f;
-                particles[i].velocity.y = GetRandomValue(10, 100) / 10.0f;
+                if (GetGamepadAxisMovement(0, 1) != 0.0f) {
+                    particles[i].radius = abs(GetRandomValue(3, 5) * max(0.55, abs(GetGamepadAxisMovement(0, 1))));
+                } else {
+                    particles[i].radius = abs(GetRandomValue(3, 5) * GetGamepadAxisMovement(0, 1));
+                }
+                particles[i].velocity.x = (GetRandomValue(-30, 30) / 10.0f);
+                particles[i].velocity.y = (GetRandomValue(0, 100) / 10.0f);
                 particles[i].position.x = rect.x;
                 particles[i].position.y = rect.y;
                 particles[i].shrinkRate = GetRandomValue(1, 10) / 10.0f;
@@ -85,7 +92,6 @@ int main() {
         BeginDrawing();
 
         ClearBackground(WHITE);
-
 
         DrawRectanglePro(normal, (Vector2){normal.width / 2, normal.height}, angle, RED);
         DrawRectanglePro(left, (Vector2){left.width + rect.width / 2, left.height / 2}, angle, BLUE);
